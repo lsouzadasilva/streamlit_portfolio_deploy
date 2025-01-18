ed2fb5ecaed5898cb1f8ac5113d713b028af78af
Leandro Souza <161175460+lsouzadasilva@users.noreply.github.com>
2025-01-18 11:01:56 -0300

    UPDATE

--- paginas/projetosbi.py
+++ paginas/projetosbi.py
@@ -86,7 +86,7 @@ def bi():
     with col9:
         st.image(image_path_fic, caption="Nome do projeto ou empresa", width=200)
         st.link_button("Acesse",
-                    "https://app.powerbi.com/view?r=eyJrIjoiMmY5YzdhOGEtZDY0ZC00MmMwLTk1ODAtMjY1ZmRjYTZlNDJiIiwidCI6IjFmMzAxZGU1LWQ1OGEtNDQyYy1hMjgyLTgwYTFhOTkzZjA0NCJ9")
+                    "https://app.powerbi.com/view?r=eyJrIjoiY2I0NmI1MTMtMDRjZi00NTY0LThjMmYtYjk4OWUwNjMwYmYxIiwidCI6IjFmMzAxZGU1LWQ1OGEtNDQyYy1hMjgyLTgwYTFhOTkzZjA0NCJ9")
 
     with col10:
         st.image(image_path_fic, caption="Nome do projeto ou empresa", width=200)
