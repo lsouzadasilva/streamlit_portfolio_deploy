84c7af45f39ad3fb57fae45d131e7c44539e476b
Leandro Souza <161175460+lsouzadasilva@users.noreply.github.com>
2025-01-18 10:36:49 -0300

    update

--- paginas/projetosbi.py
+++ paginas/projetosbi.py
@@ -31,23 +31,23 @@ def bi():
     col1, col2, col3, col4 = st.columns(4)
 
     with col1:
-        st.image(image_path_adiante, caption="Adiante Locações de pesados", width=200)
+        st.image(image_path_fic, caption="Nome do projeto ou empresa", width=200)
         st.link_button("Acesse",
                     "https://app.powerbi.com/view?r=eyJrIjoiMDJmNmVjZmYtZmFmYS00ODg4LWI4NjctYzE1ODI0OGY5NWY4IiwidCI6IjFmMzAxZGU1LWQ1OGEtNDQyYy1hMjgyLTgwYTFhOTkzZjA0NCJ9")
 
     with col2:
-        st.image(image_path_mb, caption="Mercedes Benz", width=200)
+        st.image(image_path_fic, caption="Nome do projeto ou empresa", width=200)
         st.link_button("Acesse",
                     "https://app.powerbi.com/reportEmbed?reportId=8a90ede4-e194-4dd6-8166-ee6ed6dfe13e&autoAuth=true&ctid=1f301de5-d58a-442c-a282-80a1a993f044")
 
     with col3:
-        st.image(image_path_lm, caption="LM Transportes", width=200)
+        st.image(image_path_fic, caption="Nome do projeto ou empresa", width=200)
         st.link_button("Acesse",
                     "https://app.powerbi.com/view?r=eyJrIjoiYmRiNWNjMWYtZDI0YS00N2IxLTlkOWEtYWViOGM1N2I2NTUyIiwidCI6IjFmMzAxZGU1LWQ1OGEtNDQyYy1hMjgyLTgwYTFhOTkzZjA0NCJ9")
 
 
     with col4:
-        st.image(image_path_tb, caption="Tribanco(NPS/CSAT)", width=200)
+        st.image(image_path_fic, caption="Nome do projeto ou empresa", width=200)
         st.link_button("Acesse",
                     "https://app.powerbi.com/view?r=eyJrIjoiNjAxYjIxYjktMjczOC00NWRiLWI2NjQtNmEzNGVmMzM1NmEyIiwidCI6IjFmMzAxZGU1LWQ1OGEtNDQyYy1hMjgyLTgwYTFhOTkzZjA0NCJ9")
 
@@ -58,22 +58,22 @@ def bi():
 
 
     with col5:
-        st.image(image_path_vw, caption="Volkswagen Caminhões & Ônibus", width=200)
+        st.image(image_path_fic, caption="Nome do projeto ou empresa", width=200)
         st.link_button("Acesse",
                     "https://app.powerbi.com/view?r=eyJrIjoiMWE4YzM3ODAtNzdmOS00OWU0LWFlM2ItMjRhZTVkODNjM2IwIiwidCI6IjFmMzAxZGU1LWQ1OGEtNDQyYy1hMjgyLTgwYTFhOTkzZjA0NCJ9")
 
     with col6:
-        st.image(image_path_byd, caption="BYD", width=200)
+        st.image(image_path_fic, caption="Nome do projeto ou empresa", width=200)
         st.link_button("Acesse",
                     "https://app.powerbi.com/view?r=eyJrIjoiYTM0OTUwMGYtMmJjYS00ZTVhLTlkMTktMTQzYzQzM2VmN2I3IiwidCI6IjFmMzAxZGU1LWQ1OGEtNDQyYy1hMjgyLTgwYTFhOTkzZjA0NCJ9")
         
     with col7:
-        st.image(image_path_localiza, caption="Localiza Caminhões Usados", width=200)
+        st.image(image_path_fic, caption="Nome do projeto ou empresa", width=200)
         st.link_button("Acesse",
                     "https://app.powerbi.com/view?r=eyJrIjoiNWU1NWFjNDctNGMyZS00ZGJkLTk5MGEtODVkM2E4YTU5YjllIiwidCI6IjFmMzAxZGU1LWQ1OGEtNDQyYy1hMjgyLTgwYTFhOTkzZjA0NCJ9")
 
     with col8:
-        st.image(image_path_zelo, caption="Grupo Zelo", width=200)
+        st.image(image_path_fic, caption="Nome do projeto ou empresa", width=200)
         st.link_button("Acesse",
                     "https://app.powerbi.com/view?r=eyJrIjoiMjg1ZDc5ZDItNDlhNi00NGQyLTk1MTMtNzAxNTU3NzY2N2EyIiwidCI6IjFmMzAxZGU1LWQ1OGEtNDQyYy1hMjgyLTgwYTFhOTkzZjA0NCJ9")
         
@@ -84,11 +84,11 @@ def bi():
     col9, col10 = st.columns(2)
 
     with col9:
-        st.image(image_path_indecx, caption="Monitoramento de veiculos eletricos", width=200)
+        st.image(image_path_fic, caption="Nome do projeto ou empresa", width=200)
         st.link_button("Acesse",
                     "https://app.powerbi.com/view?r=eyJrIjoiMmY5YzdhOGEtZDY0ZC00MmMwLTk1ODAtMjY1ZmRjYTZlNDJiIiwidCI6IjFmMzAxZGU1LWQ1OGEtNDQyYy1hMjgyLTgwYTFhOTkzZjA0NCJ9")
 
     with col10:
-        st.image(image_path_indecx, caption="Relatorio de entregas", width=200)
+        st.image(image_path_fic, caption="Nome do projeto ou empresa", width=200)
         st.link_button("Acesse",
                     "https://app.powerbi.com/view?r=eyJrIjoiMTk1NzI4ZDAtNjljYS00ZTllLWI0MzMtYWNiMjcwN2E5ZWYxIiwidCI6IjFmMzAxZGU1LWQ1OGEtNDQyYy1hMjgyLTgwYTFhOTkzZjA0NCJ9")
