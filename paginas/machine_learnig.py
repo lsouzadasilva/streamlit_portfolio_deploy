ed2d679e3f1c0f2294a3f07df02b3b31781ef46d
Leandro Souza <161175460+lsouzadasilva@users.noreply.github.com>
2025-03-28 16:44:11 -0300

    Update machine_learnig.py

--- paginas/machine_learnig.py
+++ paginas/machine_learnig.py
@@ -8,10 +8,15 @@ def machine():
     image_path_machine = "image/stremlit_plotly_sklearning.png"
     
     
-    col1, = st.columns(1)
+    col1, col2 = st.columns(2)
     
     
     with col1:
         st.image(image_path_machine, caption="Predição de Categorias de Produtos com Algoritmos de Machine Learning", width=200)
         st.link_button("Acesse.",
                        "https://machinelearning-classi-pro.streamlit.app/")
+
+    with col2:
+        st.image(image_path_machine, caption="Classificação de Risco de Crédito: Prevendo o Perfil de Crédito com Machine Learning", width=200)
+        st.link_button("Acesse.",
+                       "https://classificacao-score-credito.streamlit.app/")
